53dde15956ff4cd8f61459f791ea2adfd4091c30
Jaganathg <30722985+jaganathg@users.noreply.github.com>
2025-08-19 17:47:34 +0200

    day_5 third commit

--- exercises/04_traits/14_outro/src/lib.rs
+++ exercises/04_traits/14_outro/src/lib.rs
@@ -23,7 +23,9 @@ impl From<u16> for SaturatingU16 {
 
 impl From<u8> for SaturatingU16 {
     fn from(value: u8) -> Self {
-        Self { value: value as u16 }
+        Self {
+            value: value as u16,
+        }
     }
 }
 
@@ -35,38 +37,47 @@ impl From<&u16> for SaturatingU16 {
 
 impl From<&u8> for SaturatingU16 {
     fn from(value: &u8) -> Self {
-        Self { value: *value as u16 }
+        Self {
+            value: *value as u16,
+        }
     }
 }
 
 impl Add for SaturatingU16 {
     type Output = Self;
     fn add(self, other: Self) -> Self {
-        Self { value: self.value.saturating_add(other.value) }
+        Self {
+            value: self.value.saturating_add(other.value),
+        }
     }
 }
 
 impl Add<u16> for SaturatingU16 {
     type Output = Self;
     fn add(self, other: u16) -> Self {
-        Self { value: self.value.saturating_add(other) }
+        Self {
+            value: self.value.saturating_add(other),
+        }
     }
 }
 
 impl Add<&u16> for SaturatingU16 {
     type Output = Self;
     fn add(self, other: &u16) -> Self {
-        Self { value: self.value.saturating_add(*other) }
+        Self {
+            value: self.value.saturating_add(*other),
+        }
     }
 }
 
 impl Add<&SaturatingU16> for SaturatingU16 {
     type Output = Self;
     fn add(self, other: &SaturatingU16) -> Self {
-        Self { value: self.value.saturating_add(other.value) }
+        Self {
+            value: self.value.saturating_add(other.value),
+        }
     }
 }
-
 
 impl PartialEq<u16> for SaturatingU16 {
     fn eq(&self, other: &u16) -> bool {
